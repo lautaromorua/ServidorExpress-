f1af8d9ef79d37f077d8f7d4a0b3ebc9b1572c85
Lautaro <88165390+lautaromorua@users.noreply.github.com>
2022-06-08 20:20:04 -0300

    Desafio servidor con express

---
+++ contenedorProductos.js
@@ -0,0 +1,98 @@
+const fs = require('fs')
+
+class Contenedor {
+    constructor(archivo){
+        this.archivo = archivo
+        fs.promises.writeFile(this.archivo, '[]');
+}
+
+    async save (objeto) {
+        
+            let datos = await fs.promises.readFile(this.archivo, 'utf-8');
+            let arregloDatos = JSON.parse(datos);
+        try {
+            arregloDatos = arregloDatos.concat(objeto);
+            objeto.id = arregloDatos.length;
+            await fs.promises.writeFile (this.archivo, JSON.stringify(arregloDatos));
+            return objeto.id;
+        } catch (error) {
+            console.log(`Error al guardar el  archivo ${error}`);
+        }
+    };
+
+    async getById (nId) {
+        let datos = await fs.promises.readFile(this.archivo, 'utf-8');
+        let arregloDatos =JSON.parse(datos);
+        let buscarID = arregloDatos.find(({id}) => id ==nId);
+        try {
+        if (buscarID == undefined) {
+            console.log(null);
+        } else {
+            console.log(`Producto: ${nId} => ${nId.nombre}`)
+        }
+        } catch (error) {
+            console.log(`Producto no encontrado ${error}`);
+        }
+        return buscarID;
+    }
+
+    async getAll () {
+        try {
+            let datos = await fs.promises.readFile(this.archivo, 'utf-8');
+            let arregloDatos =JSON.parse(datos);
+            return arregloDatos;
+        } catch (error) {
+            console.log('Datos no encontrados');
+        }
+    }
+
+    async deleteById (nId) {
+        let datos = await fs.promises.readFile(this.archivo, 'utf-8');
+        let arregloDatos =JSON.parse(datos);
+        try {
+            let buscarID = arregloDatos.filter(({id}) => id != nId);
+            await fs.promises.writeFile(this.archivo,JSON.stringify(buscarID));
+        } catch (error) {
+            console.log(`Error al eliminar el archivo ${error}`);
+        }
+    }
+
+    async deleteAll () {
+        fs.promises.writeFile(this.archivo, '');
+        console.log('Se han borrado todos los archivos');
+    };
+}; 
+
+const productos = new Contenedor ('./productos.txt');
+
+const guardar = async () => {
+    await productos.save({
+    nombre: "Tijera",
+    precio: "80",
+    thumbnail: "tijera"
+    })
+
+    await productos.save({
+    nombre: "Lapiz",
+    precio: "25",
+    thumbnail: "lapiz"
+    })
+
+    await productos.save({
+    nombre: "Escuadra",
+    precio: "30.50",
+    thumbnail: "escuadra"
+    })
+
+    await productos.save({
+    nombre: "Fibron",
+    precio: "40",
+    thumbnail: "fibron"
+    })
+};
+
+guardar();
+
+export default class{
+    claseProductos
+}
